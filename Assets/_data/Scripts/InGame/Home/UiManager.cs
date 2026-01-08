3db58191c617667604335f14bd7a0d780247c985
Long Vo Xuan <longvx@AS.LOCAL>
2026-01-08 13:36:07 +0700

    UI Shop

--- Assets/_data/Scripts/InGame/Home/UiManager.cs
+++ Assets/_data/Scripts/InGame/Home/UiManager.cs
@@ -4,16 +4,34 @@ public class UiManager : Singleton<UiManager>
 {
     public GameObject panelHome;
     public GameObject panelGamePlay;
+    public GameObject panelShop;
+    public GameObject effEndGame;
 
     public void SceneHome()
     {
         panelHome.SetActive(true);
         panelGamePlay.SetActive(false);
+        panelShop.SetActive(false);
+        effEndGame.SetActive(true);
+        effEndGame.SetActive(false);
     }
 
     public void SceneGamePlay()
     {
         panelGamePlay.SetActive(true);
         panelHome.SetActive(false);
+        panelShop.SetActive(false);
+        effEndGame.SetActive(false);
+    }
+
+    public void SceneShop()
+    {
+        panelShop.SetActive(true);
+        effEndGame.SetActive(false);
+    }
+
+    public void ActiceEffEndGame()
+    {
+        effEndGame.SetActive(true);
     }
 }
